63f602c37e70bb096e8b9f573b7bffc59e717e0a
Farhan Khan <farhank023102@gmail.com>
2025-12-07 17:45:31 +0500

    initial commit

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 // create a ratelimiter that allows 100 request per 60 seconds
 const ratelimit = new Ratelimit({
     redis:Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5, "20 s"),
+    limiter: Ratelimit.slidingWindow(100, "60 s"),
 
 });
 
